ec626f8d4a2973937083994e9fe16e5ae3879c05
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2016-09-29 20:48:05 +0530

    Save local changes

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ git commit -am "Save local changes"
 git checkout -B gh-pages
 git add -f dist
 git commit -am "Rebuild website"
-git filter-branch -f --prune-empty --subdirectory-filter build
+git filter-branch -f --prune-empty --subdirectory-filter dist
 git push -f origin gh-pages
 git checkout -
